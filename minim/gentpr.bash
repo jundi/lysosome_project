680d89657805dfcb002de3d5ed24b1d5d11e36f8
h. mikkolainen <heikki.mikkolainen@gmail.com>
2016-06-22 14:38:09 +0300

    gromacs 5.1.2

--- minim/gentpr.bash
+++ minim/gentpr.bash
@@ -1,20 +1,14 @@
 #!/bin/bash
-#SBATCH -J gentpr_minim
-#SBATCH -n 1
-#SBATCH -t 00:05:00
 
 # input files
 mdp="minim.mdp"
 top="../../topology/topol_FEP.top"
 ndx="../../index.ndx"
+confdir="../init/"
 
 for l in {0..15}; do
-
   mkdir -p lambda$l
-
-  # Create run input file
   sed "s/init-lambda-state = 0/init-lambda-state = $l/" $mdp > lambda$l/$mdp
-  grompp -f lambda$l/$mdp -c lambda${l}/confin.gro -p $top -n $ndx -o lambda$l/topol.tpr -po lambda$l/mdout.mdp
-
+  gmx grompp -f lambda$l/$mdp -c ${confdir}/lambda${l}.gro -p $top -n $ndx -o lambda$l/topol.tpr -po lambda$l/mdout.mdp
 done
 
--- prod/batch-sisu.sh
+++ prod/batch-sisu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 #SBATCH -J prod
 #SBATCH -p small
-#SBATCH -t 12:00:00
+#SBATCH -t 5:00:00
 #SBATCH -N 16
 #SBATCH --ntasks-per-node=24
 #SBATCH --no-requeue
@@ -10,10 +10,11 @@
 
 #module load gromacs/5.0.5
 #module load gromacs/5.1.1
-export OMP_NUM_THREADS=1
-
-let ncores=$SLURM_NNODES*24
 #mdrun_bin=mdrun_mpi
 mdrun_bin=/homeappl/home/mikkolai/appl_sisu/gromacs-5.1.2/bin/mdrun_mpi
 
-aprun -n $ncores $mdrun_bin -maxh 12 -dlb yes -cpi -replex 500 -multidir lambda0 lambda1 lambda2 lambda3 lambda4 lambda5 lambda6 lambda7 lambda8 lambda9 lambda10 lambda11 lambda12 lambda13 lambda14 lambda15 
+export OMP_NUM_THREADS=1
+let ncores=$SLURM_NNODES*24
+
+aprun -n $ncores $mdrun_bin -maxh 5 -dlb yes -cpi -replex 500 -multidir lambda0 lambda1 lambda2 lambda3 lambda4 lambda5 lambda6 lambda7 lambda8 lambda9 lambda10 lambda11 lambda12 lambda13 lambda14 lambda15  -npme 4
+cp lambda0/md.log $SLURM_JOB_ID.log
