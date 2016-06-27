caef94aea97a258f519ad60c61f053a9328b4333
h. mikkolainen <heikki.mikkolainen@gmail.com>
2016-06-27 16:03:33 +0300

    set fep_residue

--- init/gengro.bash
+++ init/gengro.bash
@@ -5,7 +5,8 @@
 traj="../../npt/traj_comp.xtc"
 tpr="../../npt/topol.tpr"
 timesteps=(100000 65000 30000)
-residues=(91 92 93 94 95)
+fep_residue=91
+residues=(96 97 98 99 100)
 
 l=0
 for t in ${timesteps[@]}; do
@@ -14,7 +15,7 @@ for t in ${timesteps[@]}; do
   echo "System" | gmx trjconv -f $traj -s $tpr -dump $t -b $t -e $t -o ${t}/original.gro
 
   for r in ${residues[@]}; do
-    gro_reorder_residues.py -f ${t}/original.gro -o ${t}/${r}.gro -r1 ${residues[0]} -r2 $r
+    gro_reorder_residues.py -f ${t}/original.gro -o ${t}/${r}.gro -r1 $fep_residue -r2 $r
     ln -s ${t}/${r}.gro lambda$l.gro
     let l=$l+1
   done
