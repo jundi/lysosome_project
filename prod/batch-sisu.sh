2fa0a30509991e18969168e5f68eaa1c3a5da35b
h. mikkolainen <heikki.mikkolainen@gmail.com>
2016-07-05 14:42:29 +0300

    multidir

--- prod/batch-sisu.sh
+++ prod/batch-sisu.sh
@@ -16,5 +16,5 @@ mdrun_bin=/homeappl/home/mikkolai/appl_sisu/gromacs-5.1.2/bin/mdrun_mpi
 export OMP_NUM_THREADS=1
 let ncores=$SLURM_NNODES*24
 
-aprun -n $ncores $mdrun_bin -maxh 5 -dlb yes -cpi -replex 500 -multidir lambda0 lambda1 lambda2 lambda3 lambda4 lambda5 lambda6 lambda7 lambda8 lambda9 lambda10 lambda11 lambda12 lambda13 lambda14 lambda15  -npme 4
+aprun -n $ncores $mdrun_bin -maxh 5 -dlb yes -cpi -replex 500 -multidir lambda[0-15] -npme 4
 cp lambda0/md.log $SLURM_JOB_ID.log
