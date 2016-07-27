2279de414004d5eb4f51c22d1275017782f3c044
h. mikkolainen <heikki.mikkolainen@gmail.com>
2016-07-27 16:31:56 +0300

    sed lambda

--- fep_template/minim/gentpr.bash
+++ fep_template/minim/gentpr.bash
@@ -8,7 +8,7 @@ confdir="../init/"
 
 for l in {0..15}; do
   mkdir -p lambda$l
-  sed "s/init-lambda-state = 0/init-lambda-state = $l/" $mdp > lambda$l/$mdp
+  sed "s/init-lambda-state	= 0/init-lambda-state	= $l/" $mdp > lambda$l/$mdp
   gmx grompp -f lambda$l/$mdp -c ${confdir}/lambda${l}.gro -p $top -n $ndx -o lambda$l/topol.tpr -po lambda$l/mdout.mdp
 done
 
