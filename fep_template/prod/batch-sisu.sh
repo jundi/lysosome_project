6487be2f053a3b56f3bf2b675148678fbd95b1bb
h. mikkolainen <heikki.mikkolainen@gmail.com>
2016-08-02 13:48:46 +0300

    nsteps

--- fep_template/prod/batch-sisu.sh
+++ fep_template/prod/batch-sisu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 #SBATCH -J prod
 #SBATCH -p small
-#SBATCH -t 5:00:00
+#SBATCH -t 12:00:00
 #SBATCH -N 16
 #SBATCH --ntasks-per-node=24
 #SBATCH --no-requeue
@@ -13,5 +13,5 @@ module load gromacs/5.1.2
 export OMP_NUM_THREADS=2
 let ncores=$SLURM_NNODES*24
 
-aprun -j 2 -d 2 -n $ncores $mdrun_bin -maxh 5 -dlb yes -cpi -replex 500 -multidir lambda{0..15} -npme 4
+aprun -j 2 -d 2 -n $ncores gmx_mpi mdrun -dlb yes -cpi -replex 500 -multidir lambda{0..15} -npme 4 -nsteps 7500000
 cp lambda0/md.log $SLURM_JOB_ID.log
