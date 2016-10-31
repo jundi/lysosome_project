aa24108975a462db4e1360d08c2bc3d4dc2c17ee
h. mikkolainen <heikki.mikkolainen@gmail.com>
2016-10-31 15:01:10 +0200

    long NPT

--- fep_template/init/gengro.bash
+++ fep_template/init/gengro.bash
@@ -1,10 +1,11 @@
 #!/bin/bash
+set -e
 
 # use different timesteps and residues to produce different inital states for
 # FEP+REMD simulations
-traj="../../npt/traj_comp.xtc"
-tpr="../../npt/topol.tpr"
-timesteps=(100000 70000 40000)
+traj="../../../../npt/traj_comp.xtc"
+tpr="..././../../npt/topol.tpr"
+timesteps=(500000 400000 300000 200000)
 fep_residue=91
 residues=(96 97 98 99 100)
 
