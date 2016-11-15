26d1ee0ab3f4f20c3edd60cf09ee5e01864c2bfd
h. mikkolainen <heikki.mikkolainen@gmail.com>
2016-11-15 12:07:15 +0200

    LBPA14/16

--- batch_cumsum.sh
+++ batch_cumsum.sh
@@ -2,45 +2,31 @@
 set -e 
 
 systems=( \
-#150POPC_20CHOL_30CERA \
-#150POPC_20CHOL_30LBPA22RR \
-#150POPC_20CHOL_30LBPA22SS \
-#150POPC_20CHOL_30SM16 \
-#169POPC_1CHOL_30CERA \
-#169POPC_1CHOL_30LBPA22RR \
-#169POPC_1CHOL_30LBPA22SS \
-#169POPC_1CHOL_30SM16 \
-#180POPC_20CHOL \
-#199POPC_1CHOL \
-#20CHOL_180CERA \
-#20CHOL_180SM16 \
-#74POPC_10CHOL_16LBPA22RR \
-#74POPC_10CHOL_16LBPA22SS \
-#90POPC_10CHOL/free_energy/analys/bar_b100000/1-100000 \
-#90POPC_10CHOL/free_energy2/analys/bar_b100000/1-100000 \
-#60POPC_10CHOL_30CERA/free_energy/analys/bar_b100000/1-100000 \
-#60POPC_10CHOL_30CERA/free_energy2/analys/bar_b100000/1-100000 \
-#60POPC_10CHOL_30CERA/free_energy3/analys/bar_b100000/1-100000 \
-#60POPC_10CHOL_30DPPC/free_energy/analys/bar_b100000/1-100000 \
-#60POPC_10CHOL_30DPPC/free_energy2/analys/bar_b100000/1-100000 \
-#60POPC_10CHOL_30SM16/free_energy/analys/bar_b100000/1-100000 \
-#60POPC_10CHOL_30SM16/free_energy2/analys/bar_b100000/1-100000 \
-#60POPC_10CHOL_30LBPA22RR/free_energy/analys/bar_b100000/1-100000 \
-#60POPC_10CHOL_30LBPA22RR/free_energy2/analys/bar_b100000/1-100000 \
-#60POPC_10CHOL_30LBPA22RR/free_energy3/analys/bar_b100000/1-100000 \
-90POPC_10CHOL/free_energy/average \
-60POPC_10CHOL_30CERA/free_energy/average \
-60POPC_10CHOL_30DPPC/free_energy/average \
-60POPC_10CHOL_30SM16/free_energy/average \
-60POPC_10CHOL_30LBPA22RR/free_energy/average \
+60POPC_10CHOL_30CERA \
+60POPC_10CHOL_30DPPC \
+60POPC_10CHOL_30LBPA14 \
+60POPC_10CHOL_30LBPA16 \
+60POPC_10CHOL_30LBPA22RR \
+60POPC_10CHOL_30SM16 \
+90POPC_10CHOL \
+)
+
+fepdirs=( \
+"free_energy/leaflet_A/analys/bar_b100000/1-100000" \
+"free_energy/leaflet_B/analys/bar_b100000/1-100000" \
+"free_energy/average" \
 )
 
 for s in ${systems[@]}; do
   echo $s
 
-  cd $s
-  xvg_cumsum.py -f bar.xvg -o bar_cumsum.xvg 
-  xvg_cumsum.py -f bar.xvg -o bar_cumsum_SSE.xvg -c 1 -e 2
-  cd -
+  for d in ${fepdirs[@]}; do
+    cd $s/$d
+    pwd
+    xvg_cumsum.py -f bar.xvg -o bar_cumsum.xvg 
+    xvg_cumsum.py -f bar.xvg -o bar_cumsum_SSE.xvg -c 1 -e 2
+    cd -
+  done
 
 done
+
